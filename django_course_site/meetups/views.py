d21fc3d9a372825b25dd972e3dfd6ae530526a71
nielsRocholl <nielsrocholl@gmail.com>
2021-12-19 19:55:38 +0100

    added db model

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,32 +1,23 @@
 from django.shortcuts import render
+from .models import Meetup
 
 
 def index(request):
-    meetups = [
-        {
-            'title': 'A First Meetup',
-            'location': 'New York',
-            'slug': 'a-first-meetup'
-        },
-        {
-            'title': 'A second Meetup',
-            'location': 'Paris',
-            'slug': 'a-second-meetup'
-        },
-
-    ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
-        'show_meetups': False,
         'meetups': meetups
     })
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {'title': 'A First Meetup',
-                       'description': 'This is the first meetup!',
-                       }
-
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description']
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description
+        })
+    except Exception as exc:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
+        })
